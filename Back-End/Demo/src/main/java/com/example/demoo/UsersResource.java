7b5c851f19838373b8c6e6a7ddc9663d27581040
Damien Eshimu <damien.eshimu@outlook.com>
2023-02-10 14:29:39 +0000

    Demo setup

---
+++ Back-End/Demo/src/main/java/com/example/demoo/UsersResource.java
@@ -0,0 +1,11 @@
+package com.example.demoo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class UsersResources {
+    
+}
