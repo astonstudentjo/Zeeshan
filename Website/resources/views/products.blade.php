660f2f8303d83b6ef71d37a0628a4f174f126bb3
Drkongy <80266694+Drkongy@users.noreply.github.com>
2023-02-16 00:10:53 +0000

    Made the Products page images smaller.

--- Website/resources/views/products.blade.php
+++ Website/resources/views/products.blade.php
@@ -15,10 +15,17 @@
 
             <div>
                 <a href = "/products/{{$product->id}}">
-                <img src = "/productImages/{{$product->img}}"></div>
-                <h4> {{$product->category}}</h4>
+                
+
+
+                <img src="/productImages/{{ $product->img }}" width="300px">
+                
+            </div>
+                <h1> {{$product->name}}</h1>
                 <h1>{{$product->artist}}</h1>
-                <h4> {{$product->name}}</h4>
+                <h4> {{$product->category}}</h4>
+
+
                 </a>
             </div>
         @endforeach
