8f5e1e72d7673acf8d5d46d15010a2c8f37ee49d
ihaseebahmed <116155266+ihaseebahmed@users.noreply.github.com>
2023-02-23 19:53:46 +0000

    comitting mapper function again

--- Back-End/demoo/src/main/java/com/example/demoo/resource/DemooApplication.java
+++ Back-End/demoo/src/main/java/com/example/demoo/resource/DemooApplication.java
@@ -1,8 +1,14 @@
-package com.example.demoo;
+package com.example.demoo.resource;
 
+import org.apache.ibatis.type.MappedTypes;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demoo.model.Product;
+
+@MappedTypes(Product.class)
+@MapperScan
 @SpringBootApplication
 public class DemooApplication {
 
--- Back-End/demoo/src/main/java/com/example/demoo/resource/ProductResource.java
+++ Back-End/demoo/src/main/java/com/example/demoo/resource/ProductResource.java
@@ -1,4 +1,6 @@
-package com.example.demoo;
+package com.example.demoo.resource;
+
+import java.util.List;
 
 // import java.util.List;
 
@@ -6,16 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demoo.model.Products;
+import com.example.demoo.mapper.ProductMapper;
+import com.example.demoo.model.Product;
 
 @RestController
-@RequestMapping
+@RequestMapping("/rest/Product")
 
 public class ProductResource {
     
+    private ProductMapper productMapper;
+
+    public ProductResource(ProductMapper productMapper) {
+        this.productMapper = productMapper;
+    }
     @GetMapping("/all")
-    public List<Products> getAll() {
-        
+    public List<Product> getAll() {
+       return productMapper.findAll();
     }
  
 }
