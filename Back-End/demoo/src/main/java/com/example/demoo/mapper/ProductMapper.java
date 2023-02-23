03d2be921b2aa13350f55c053876f98e60e09c4d
ihaseebahmed <116155266+ihaseebahmed@users.noreply.github.com>
2023-02-23 19:56:36 +0000

    product mapper
    added product mapper and also changed the location of the demoapplication along with the product resource in a folder named resource found in the main java folder

---
+++ Back-End/demoo/src/main/java/com/example/demoo/mapper/ProductMapper.java
@@ -0,0 +1,13 @@
+package com.example.demoo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demoo.model.Product;
+//this is the mapper
+@Mapper
+public interface ProductMapper {
+    
+    List<Product> findAll();
+}
