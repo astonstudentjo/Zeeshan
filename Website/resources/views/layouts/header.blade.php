12d3767493864cf23a9a5312174592e68762d83d
Drkongy <80266694+Drkongy@users.noreply.github.com>
2023-03-02 18:26:51 +0000

    Fixed the Hierachy so no one gets confused.

--- Website/resources/views/layouts/header.blade.php
+++ Website/resources/views/layouts/header.blade.php
@@ -1,57 +1,72 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
 
-        <link rel="stylesheet" href="../css/header.css">
+<head>
+    <meta charset="utf-8">
 
-
-                
-    </head>
-        
-
-        <div class = "container">
-            <div class = "left-header">
-                <a>EventTick</a>
-                <a href = "/">Home</a>
-                <a href = "/products">Products</a>
-                <a href = "/aboutus">About Us</a>
-                <a href = "/contactus">Contact Us</a>
-            </div>
+    <!-- <link rel="stylesheet" href="../css/header.css"> -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
 
 
-            @guest
-           
-           <div class = "right-header">
-               <a href = "/basket">Basket</a>
-               <a href = "/signup">Sign Up</a>
-               <a href = "/login">Login</a>
-           </div>
-   
-           @endguest
 
-           @auth
-            
-        
-           
-           <div class = "right-header">
-                <a href = "/basket">Basket</a>
-                <a href = "/orders">Orders</a>
-
-                 
-                <form action = "/signout" method = "post">
-                    @csrf
-                    <a>{{auth()->user()->name}}</a>
-                    <button type = "submit" >Sign Out</button>
-                </form>
-                <a href = "/signout">
-            </div>
-        
-            @endauth
+    <style>
+        /* .container {
+            display: flex;
+            justify-content: space-between;
+            padding: 10px;
+            background-color: lightblue;
+        } */
+    </style>
 
 
-        </div>
-    <body>
-        @yield('body')
-    </body>
+
+</head>
+
+
+<div class="container">
+    <div class="left-header">
+        <a>EventTick</a>
+        <a href="/">Home</a>
+        <a href="/products">Products</a>
+        <a href="/aboutus">About Us</a>
+        <a href="/contactus">Contact Us</a>
+    </div>
+
+
+    @guest
+
+    <div class="right-header">
+        <a href="/basket">Basket</a>
+        <a href="/signup">Sign Up</a>
+        <a href="/login">Login</a>
+    </div>
+
+    @endguest
+
+    @auth
+
+
+
+    <div class="right-header">
+        <a href="/basket">Basket</a>
+        <a href="/orders">Orders</a>
+
+
+        <form action="/signout" method="post">
+            @csrf
+            <a>{{auth()->user()->name}}</a>
+            <button type="submit">Sign Out</button>
+        </form>
+        <a href="/signout">
+    </div>
+
+    @endauth
+
+
+</div>
+
+<body>
+    @yield('body')
+</body>
+
 </html>
--- Website/resources/views/products.blade.php
+++ Website/resources/views/products.blade.php
@@ -1,51 +1,127 @@
 <!DOCTYPE html>
-@extends('layouts.header')
-@section('body')
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ticket Selling Website</title>
 
 
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Products</title>
-        <style>
 
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="../css/products.css">
+
+    
+<style>
         
-        </style>
-    </head>
+    </style>
+</head>
+<body>
 
-    <div class = "categories">
-        <a href = '/products/category/price_ascending'>Price-Low to High</a>
-        <a href = '/products/category/price_descending'>Price-High to Low</a>
-        <a href = '/products/category/pop'>Pop</a>
-        <a href = '/products/category/metal'>Metal</a>
-        <a href = '/products/category/rap'>Rap</a>
-        <a href = '/products/category/rock'>Rock</a>
-        <a href = '/products/category/country'>Country</a>
+<!-- header section starts  -->
 
+<header>
+</header>
 
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3>Tickets Available!</h3>
+        <span> Concerts & music events </span>
+        <p>Attention all music lovers! Get ready to experience the thrill of live music!</p>
+        <p>We are excited to announce that tickets to the event are now on sale! Don't miss the chance to see your favorite artists perform live on stage, jamming out to the tunes that have become the soundtrack of our lives.
+        With a lineup of world-class musicians, this event is sure to be a night to remember. Whether you're a fan of rock, pop, hip-hop,</p>
+        <a href="#products" class="btn">buy now</a>
     </div>
-    <div class = "products">
-        @foreach($products as $product)
+    
+</section>
 
-            <div class = "product">
-                <a href = "/products/{{$product->id}}">               
-                <img src="/productImages/{{ $product->img }}" width="300px">
-                <h1> {{$product->name}}</h1>
-                <h1>{{$product->artist}}</h1>
-                <h4> {{$product->category}}</h4>
-                <h4> £{{$product->price}}</h4>
-                @if ($product->stock <= 0)
+<!-- home section ends -->
 
-                <h4>Out of Stock!</h4>
-                @elseif ($product-> stock< 20)
-                <h4>Only {{$product -> stock}} remaining buy now!!!</h4>  
-                @endif
+<section class="filters">
+<div class="bar">
+          
+    <p> Search Event </p>
+    <p> place </p>
+    <p> Time</p>
+
+</div>
+</section>
+
+<!-- icons section starts  -->
+
+<section class="icons-container">
 
 
-                </a>
+    <div class="icons">
+        <img src="../images/icon-2.png" alt="">
+        <div class="info">
+            <h3>10 days returns</h3>
+            <span>moneyback guarantee</span>
+        </div>
+    </div>
+
+    <div class="icons">
+        <img src="../images/icon-3.png" alt="">
+        <div class="info">
+            <h3>offer & gifts</h3>
+            <span>on all orders</span>
+        </div>
+    </div>
+
+    <div class="icons">
+        <img src="../images/icon-4.png" alt="">
+        <div class="info">
+            <h3>secure paymens</h3>
+            <span>protected by paypal</span>
+        </div>
+    </div>
+   
+</section>
+
+<!-- icons section ends -->
+
+<!-- prodcuts section starts  -->
+
+<section class="products" id="products">
+
+    <h1 class="heading"> TICKETS AVAILABLE </h1>
+
+ <div class="box-container">
+
+
+    @foreach($products as $product)
+        <div class="box">
+            <span class="discount">-10%</span>
+            <div class="image">
+                <img src="../images/tickets.jpg" alt="">
+                <div class="icons">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="cart-btn">add to cart</a>
+                    <a href="#" class="fas fa-share"></a>
+                </div>
             </div>
-        @endforeach
-    </div>
-</html>
+            <div class="content">
+                <h3>Product 1</h3>
+                <h2> {{$product->artist}}</h2>
+                <h2>{{$product->category}}</h2>
+                <p> {{$product->description}}</p>
+                <div class="price"> $12.99 <span>$15.99</span> </div>
+            </div>
+        </div>
+    @endforeach
 
-@endsection
+        </div>
+</section>
+
+<!-- prodcuts section ends -->
+
+</body>
+</html>
