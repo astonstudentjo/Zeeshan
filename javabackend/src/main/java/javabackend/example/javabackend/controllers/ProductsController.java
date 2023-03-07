04bfecff5c81530ffe096d4738665d41215474e1
Muniib-Ali <muniib11@live.com>
2023-03-07 14:42:53 +0000

    Concert images correctly saved

--- javabackend/src/main/java/javabackend/example/javabackend/controllers/ProductsController.java
+++ javabackend/src/main/java/javabackend/example/javabackend/controllers/ProductsController.java
@@ -6,12 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import javabackend.example.javabackend.repositories.ProductsRepository;
 import javabackend.example.javabackend.Service.ProductsService;
 import javabackend.example.javabackend.models.Products;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 
 @Controller
 public class ProductsController {
@@ -44,11 +55,34 @@ public class ProductsController {
         return "Create-product";
         
     }
-
+   
 
     @PostMapping("/products")
-    public String saveProduct(@ModelAttribute("products") Products products){
-        productsService.saveProduct(products);
+    public String saveProduct(@ModelAttribute(name = "products") Products products, @RequestParam("imgUpload") MultipartFile multipartFile) throws IOException{
+
+
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        products.setImg(fileName);
+        Products savedProduct = productsService.saveProduct(products);
+        String uploadDirectory = "../Zeeshan/Website/public/productImages";
+        Path uploadPath = Paths.get(uploadDirectory);
+        if(!Files.exists(uploadPath)){
+            Files.createDirectories(uploadPath);
+        }
+        
+
+
+        try(InputStream inputStream = multipartFile.getInputStream()){
+
+        Path filePath = uploadPath.resolve(fileName);
+        Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+
+        } catch(IOException e){
+            throw new IOException("Could not save uploaded file " + fileName);
+        }
+        
+        
+        
         return "redirect:/products";
 
     }
@@ -57,6 +91,8 @@ public class ProductsController {
     public String deleteProduct(@PathVariable Integer id ){
         productsService.deleteProductById(id);
         return "redirect:/products";
+        
 
     }
+    
 }
