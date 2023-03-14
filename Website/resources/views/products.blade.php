b0d5a87f1e9f27894af3bd21507d48d54713db20
Jenson <100025674+Gamercave@users.noreply.github.com>
2023-03-14 15:41:09 +0000

    Home Page mostly done could do with darkeneing hole image or changing it
    CSS and home page HTML eddited

--- Website/resources/views/products.blade.php
+++ Website/resources/views/products.blade.php
@@ -14,18 +14,7 @@
 
 <body>
 
-    <section class="home" id="home">
-
-        <div class="content">
-            <h3>Tickets Available!</h3>
-            <span> Concerts & music events </span>
-            <p>Attention all music lovers! Get ready to experience the thrill of live music!</p>
-            <p>We are excited to announce that tickets to the event are now on sale! Don't miss the chance to see your favorite artists perform live on stage, jamming out to the tunes that have become the soundtrack of our lives.
-                With a lineup of world-class musicians, this event is sure to be a night to remember. Whether you're a fan of rock, pop, hip-hop,</p>
-            <a href="#products" class="btn">buy now</a>
-        </div>
-
-    </section>
+    
 
     <!-- home section ends -->
 
