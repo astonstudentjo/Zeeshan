b2f28dc1c6bcf951307ee5bb4a783892425b4a23
Muniib-Ali <muniib11@live.com>
2023-03-15 12:42:49 +0000

    Unit tests created for authenticated users

--- Website/tests/Unit/AllUserTest.php
+++ Website/tests/Unit/AllUserTest.php
@@ -18,36 +18,42 @@ class AllUserTest extends TestCase
         $this->assertTrue(true);
     }
 
+    //Tests that all users can access the products page
     public function test_products_route(){
         $response = $this->get('/products');
 
         $response->assertStatus(200);
     }
 
+    //Tests that all users can access the first product
     public function test_firstproduct_route(){
         $response = $this->get('/products/1');
 
         $response->assertStatus(200);
     }
 
+    //Tests that all users can access the last product
     public function test_lastproduct_route(){
         $response = $this->get('/products/25');
 
         $response->assertStatus(200);
     }
 
+    //Tests that all users can access the aboutus page
     public function test_aboutus_route(){
         $response = $this->get('/aboutus');
 
         $response->assertStatus(200);
     }
 
+    //Tests that all users can access the contactus page
     public function test_contactus_route(){
         $response = $this->get('/contactus');
 
         $response->assertStatus(200);
     }
     
+    //Tests that all users can access the basket page
     public function test_basket_route(){
         $response = $this->get('/basket');
 
--- Website/tests/Unit/GuestTest.php
+++ Website/tests/Unit/GuestTest.php
@@ -16,24 +16,28 @@ class GuestTest extends TestCase
         $this->assertTrue(true);
     }
 
+    //tests that guests can access the signup route
     public function test_signup_route(){
         $response = $this->get('/signup');
 
         $response->assertStatus(200);
     }
 
+    //tests that guests can access the login route
     public function test_login_route(){
         $response = $this->get('/login');
 
         $response->assertStatus(200);
     }
 
+    //tests that guests are redirected after trying to acess the orders route
     public function test_orders_route(){
         $response = $this->get('/orders');
 
         $response->assertStatus(302);
     }
 
+    //tests that guests are redirected after trying to access order confirmation pages
     public function test_specificorder_route(){
         $response = $this->get('/confirmation/1');
 
