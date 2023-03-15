4a323c3e88f25962dd2f0f146026e734479cbd64
Muniib-Ali <muniib11@live.com>
2023-03-15 12:08:23 +0000

    Unit tests for all users on laravel

---
+++ Website/tests/Unit/AllUserTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Unit;
+
+
+
+use Tests\TestCase;
+
+class AllUserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_products_route(){
+        $response = $this->get('/products');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_firstproduct_route(){
+        $response = $this->get('/products/1');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_lastproduct_route(){
+        $response = $this->get('/products/25');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_aboutus_route(){
+        $response = $this->get('/aboutus');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_contactus_route(){
+        $response = $this->get('/contactus');
+
+        $response->assertStatus(200);
+    }
+    
+    public function test_basket_route(){
+        $response = $this->get('/basket');
+
+        $response->assertStatus(200);
+    }
+}
