90c0d4c93c26a683203e95b9e3e4ab48a2186d35
Zeeshan <kongking7861@gmail.com>
2023-03-17 14:28:09 +0000

    Delete javabackend directory

--- javabackend/src/main/java/javabackend/example/javabackend/repositories/ordersRepository.java
+++
@@ -1,9 +0,0 @@
-package javabackend.example.javabackend.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import javabackend.example.javabackend.models.*;
-import java.util.List;
-public interface ordersRepository extends CrudRepository<orders, Integer> {
-    List<orders> findAll();
-
-}
