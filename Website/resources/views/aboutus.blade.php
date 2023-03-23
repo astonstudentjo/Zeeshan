59084aaad76f80ccae6bcb6bad00935d3c192ce0
Jenson <100025674+Gamercave@users.noreply.github.com>
2023-03-23 16:09:23 +0000

    fixed alignment and spelling mistakes

--- Website/resources/views/aboutus.blade.php
+++ Website/resources/views/aboutus.blade.php
@@ -48,13 +48,12 @@
 <!-- about section starts  -->
 
 <body>
-    
-  <div>
+  <section>
         <img class="image" src="./images/background.png" alt="background image"> 
-   
-  </div> 
+      </section>
+      </div> 
 
-        <section>    
+        <section class = info>    
         <h2 class="heading">Who we are: </h1>  
         <div class="info">
             <p>Welcome to our E-ticketing platform! We are a team of dedicated professionals</p>
@@ -65,11 +64,10 @@
                to music. So not only are we giving you guys the chance to see some amazing artists such as Chris brown and Adele but we are giving more recognition to smaller artists who need a platform to expand and grow. </p>
     </div>
     </section>
-        <section class = "Social Media">
+        <section class = "Social_Media">
         <a class="twitter-timeline" href="https://twitter.com/event_tick?ref_src=twsrc%5Etfw">Tweets by event_tick</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </section>
     <section>
-        <div class="info">
     <div>
         <h3 class="heading"> Our team </h>
     </div>
@@ -101,21 +99,21 @@
           <!--<img src="images/img4.jpg" alt="" />-->
         </div>
         <h2>Jenson James-Ross</h2>
-        <p>Wroked on Admin and Front end</p>
+        <p>Worked on Admin and Front end</p>
       </div>
       <div class="card">
         <div class="image">
           <!--<img src="images/img4.jpg" alt="" />-->
         </div>
         <h2>Faryal Mansoor</h2>
-        <p>Wroked on Admin and Front end</p>
+        <p>Worked on Admin and Front end</p>
       </div>
       <div class="card">
         <div class="image">
           <!--<img src="images/img4.jpg" alt="" />-->
         </div>
         <h2>Zeeshan Mohammaed</h2>
-        <p>Wroked on database, laravel and version Control</p>
+        <p>Worked on database, laravel and version Control</p>
       </div>
       <div class="card">
         <div class="image">
@@ -133,9 +131,9 @@
       </div>
     </section>
     <section>
-        <div class="info">
+        
         <h4 class="heading">Our mission:</h4>
-   
+        <div class="info">
             <p>We take pride in our work, and our goal is to deliver exceptional customer service to each and 
                 every one of our customers. We are committed to making the process of buying e-tickets online as simple and effortless as possible,
                  while ensuring that all transactions are safe and secure.</p>
@@ -146,6 +144,7 @@
             <p> Thank you for choosing us as your go-to platform for all your e-ticketing needs. We look forward to continuing to serve you and
                  providing you with the best possible experience!</p>
         </div>
+      </div>
     </section>
     </body>
 
