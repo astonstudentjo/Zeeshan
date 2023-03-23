7f9b414e3899c8b3fd373c1d0a8d805e37ac9081
Drkongy <80266694+Drkongy@users.noreply.github.com>
2023-03-23 16:38:17 +0000

    confirmation done

--- Website/resources/views/confirmation.blade.php
+++ Website/resources/views/confirmation.blade.php
@@ -7,6 +7,7 @@
 <head>
     <meta charset="utf-8">
     <title>confirmation</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/confirmation.css') }}">
 </head>
 <!-- only show this data if the user_id matches the current user -->
 
@@ -14,46 +15,55 @@
 
 @if($confirmation->user_id == Auth::user()->id)
 
-<div>
-    <h1>Thank you for your order!</h1>
-    <h2>Order number: {{$confirmation->id}}</h2>
-    <h2>Order date: {{$confirmation->created_at}}</h2>
-    <h2>Order total: £{{$confirmation->total_price}}</h2>
-    <h2>Order status: {{$confirmation->status}}</h2>
-    <h2>Tickets Ordered:</h2>
-</div>
-<div>
-    <table>
-        <thead>
-            <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Quantity</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php $totalQuantity = 0; ?>
-            @foreach($confirmation->items as $orderItem)
-            <?php $totalQuantity += $orderItem->quantity; ?>
-            <tr>
-                <td>{{ $orderItem->product->name }}</td>
-                <td>£{{ $orderItem->product->price }}</td>
-                <td>{{ $orderItem->quantity }}</td>
-            </tr>
-            @endforeach
-            <tr>
-                <td><br>Total Quantity:</td>
-                <td></td>
-                <td>{{$totalQuantity}}</td>
-            </tr>
-        </tbody>
-    </table>
+<br>
+<br>
+<!-- <h1>Thank you for your order!</h1> -->
+<img src="{{ asset('images/Thanks.png') }}" alt="Thanks" width="500" height="500">
+<h2>Order number: {{$confirmation->id}}</h2>
+<h2>Order date: {{$confirmation->created_at}}</h2>
+<h2>Order total: £{{$confirmation->total_price}}</h2>
+<h2>Order status: {{$confirmation->status}}</h2>
+<br>
+<h2>Tickets Ordered:</h2>
+<table>
+    <thead>
+        <tr>
+            <th>Image</th>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $totalQuantity = 0; $totalPrice = 0; ?>
+        @foreach($confirmation->items as $orderItem)
+        <?php 
+        $totalQuantity += $orderItem->quantity; 
+        $totalPrice += $orderItem->price;
+        ?>
+        <tr>
+            <td><img src="{{ asset('images/' . $orderItem->product->img) }}" alt="product image" width="100" height="100"></td>
+            <td>{{ $orderItem->product->name }}</td>
+            <td>£{{ $orderItem->product->price }}</td>
+            <td>{{ $orderItem->quantity }}</td>
+        </tr>
+        @endforeach
+        <tr>
+            <td>Total:</td>
+            <td>£{{ number_format($totalPrice, 2) }}</td>
+            <td>{{$totalQuantity}}</td>
+        </tr>
+    </tbody>
+</table>
+
+<br>
 
 
-    <br>
-    <br>
-    <a href="/orders">View Orders</a>
-</div>
+
+<br>
+<!-- <a href="/orders">View Orders</a> --> 
+<!-- make a view orders button -->
+<a href="/orders" class="btn btn-primary">View Orders</a>
 @endif
 
 </html>
--- Website/resources/views/contactus.blade.php
+++ Website/resources/views/contactus.blade.php
@@ -4,55 +4,55 @@
 
 
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Contact Us</title>
-        <script src="https://kit.fontawesome.com/b356ad9dc8.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/contactus.css') }}">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-    </head>
+<head>
+    <meta charset="utf-8">
+    <title>Contact Us</title>
+    <script src="https://kit.fontawesome.com/b356ad9dc8.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/contactus.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+</head>
 
-    <div class = "contactus-container">
-        <div class = "contactus-area">
-        <div class = "contactus-left">
-            <div class = "contactus-details address">
+<div class="contactus-container">
+    <div class="contactus-area">
+        <div class="contactus-left">
+            <div class="contactus-details address">
 
-                <div class = "image-icon">
-                <img src="{{ asset('images/phone-icon.png') }}" width="30" height="30">
+                <div class="image-icon">
+                    <img src="{{ asset('images/phone-icon.png') }}" width="30" height="30">
                 </div>
 
-                <div class = "icon-text">
-                <h4>Address</h4>
-                <p>Aston St, Birmingham B4 7ET</p>
-                </div>
-
-
-
-            </div>
-            <div class = "contactus-details phone">
-
-                <div class = "image-icon">
-                <img src="{{ asset('images/phone-icon.png') }}" width="30" height="30">
-                </div>
-
-                <div class = "icon-text">
-                <h4>Phone</h4>
-                <p>0121 204 3000</p>
+                <div class="icon-text">
+                    <h4>Address</h4>
+                    <p>Aston St, Birmingham B4 7ET</p>
                 </div>
 
 
 
             </div>
-            <div class = "contactus-details email">
+            <div class="contactus-details phone">
 
-                <div class = "image-icon">
-                <img src="{{ asset('images/email-icon.png') }}" width="30" height="30">
+                <div class="image-icon">
+                    <img src="{{ asset('images/phone-icon.png') }}" width="30" height="30">
                 </div>
 
-                <div class = "icon-text">
-                <h4>Email</h4>
-                <p>EventTick@Aston.ac.uk</p>
+                <div class="icon-text">
+                    <h4>Phone</h4>
+                    <p>0121 204 3000</p>
+                </div>
+
+
+
+            </div>
+            <div class="contactus-details email">
+
+                <div class="image-icon">
+                    <img src="{{ asset('images/email-icon.png') }}" width="30" height="30">
+                </div>
+
+                <div class="icon-text">
+                    <h4>Email</h4>
+                    <p>EventTick@Aston.ac.uk</p>
                 </div>
 
 
@@ -60,23 +60,25 @@
 
         </div>
 
-        <div class = "contactus-right">
-            <h3 id = "form-header">Contact US</h3>
-            <p id = "form-text">Have an enquiry that needs to be answered? Please contact us by filling out the form below!</p>
+        <div class="contactus-right">
+            <h3 id="form-header">Contact US</h3>
+            <p id="form-text">Have an enquiry that needs to be answered? Please contact us by filling out the form below!</p>
             <form class="contactus-form">
 
-                    <input type="text" id="contactus-name" placeholder="Name" required>
-                    <input type="email" id="contactus-email" placeholder="Email" required>
-                    <input type="subject" id="contactus-subject" placeholder="EventTick Enquiry" required>
-                    <input type="message" id="contactus-message" placeholder="Message" required>
-                    <input type="submit" id="contactus-btn">
+                <input type="text" id="contactus-name" placeholder="Name" required>
+                <input type="email" id="contactus-email" placeholder="Email" required>
+                <input type="subject" id="contactus-subject" placeholder="EventTick Enquiry" required>
+                <input type="message" id="contactus-message" placeholder="Message" required>
+                <input type="submit" id="contactus-btn">
 
 
             </form>
 
         </div>
     </div>
-    </div>
+</div>
+
+<!-- <div class="padder"></div> -->
 
 
 
