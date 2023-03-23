b2efbdaa9d7aa6d4f29361247250e3ced5d9791b
hvmera <116155374+hvmera@users.noreply.github.com>
2023-03-23 02:26:23 +0000

    basket-unfinshed

--- Website/resources/views/basket.blade.php
+++ Website/resources/views/basket.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-@extends('layouts.main')
+
 @section('body')
 
 
@@ -108,6 +108,40 @@
 
 <div class="empty-basket-Padder"></div>
 
+<header>
+		<h1>My Basket</h1>
+	</header>
+	<main>
+		<section class="cart-items">
+			<h2>Shopping Cart</h2>
+			<ul>
+				<li>
+					<img src="https://via.placeholder.com/100x100" alt="Product Image">
+					<div class="product-info">
+						<h3>Product Name</h3>
+						<p>Price: $9.99</p>
+						<input type="number" value="1" min="1">
+						<button>Remove</button>
+					</div>
+				</li>
+				<li>
+					<img src="https://via.placeholder.com/100x100" alt="Product Image">
+					<div class="product-info">
+						<h3>Product Name</h3>
+						<p>Price: $14.99</p>
+						<input type="number" value="1" min="1">
+						<button>Remove</button>
+					</div>
+				</li>
+			</ul>
+		</section>
+		<section class="total">
+			<h2>Total</h2>
+			<p>Subtotal: $24.98</p>
+			<p>Shipping: $5.00</p>
+			<p>Total: $29.98</p>
+			<button>Checkout</button>
+		</section>
 
 
 @endsection
