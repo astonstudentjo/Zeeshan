e3cd03efeaf08d08f665d22c9b0be26d849dd27e
Drkongy <80266694+Drkongy@users.noreply.github.com>
2023-03-28 18:51:23 +0100

    Fixed gap

--- Website/resources/views/layouts/main.blade.php
+++ Website/resources/views/layouts/main.blade.php
@@ -3,10 +3,9 @@
 
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.0.7/css/boxicons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
 </head>
 
 <body>
