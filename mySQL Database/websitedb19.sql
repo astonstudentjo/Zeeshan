f2d3603508deeec249b9535ce5de6307f98e5d78
Muniib-Ali <muniib11@live.com>
2023-03-28 10:19:08 +0100

    Updated sql file

--- mySQL Database/websitedb19.sql
+++ mySQL Database/websitedb19.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2023 at 04:48 PM
+-- Generation Time: Mar 28, 2023 at 11:17 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.4
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `websitedb19`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `name` varchar(1000) NOT NULL,
+  `email` varchar(1000) NOT NULL,
+  `subject` varchar(1000) NOT NULL,
+  `message` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `name`, `email`, `subject`, `message`) VALUES
+(1, 'Muniib Ali', '200103424@aston.ac.uk', 'test', 'test'),
+(2, 'Muniib Ali', '200103424@aston.ac.uk', 'test', 'test'),
+(3, 'Muniib Ali', '200103424@aston.ac.uk', 'test', 'test');
 
 -- --------------------------------------------------------
 
@@ -77,7 +100,18 @@ CREATE TABLE `newsletter` (
 INSERT INTO `newsletter` (`id`, `email`) VALUES
 (5, 'muniib11@live.com'),
 (6, 'test@gmail.com'),
-(7, 'test@live.com');
+(7, 'test@live.com'),
+(8, 'tes12@gmail.com'),
+(9, 'test123@gmail.com'),
+(10, 'te414141st@gmail.com'),
+(11, 'tes41414141t@gmail.com'),
+(12, 'tes3141451412@gmail.com'),
+(13, 'tes141414142@gmail.com'),
+(14, 'tes4141515151t@live.com'),
+(15, 'fefadfwa@gmail.com'),
+(16, 't4514dadaes12@gmail.com'),
+(17, 'testdwad1541dda@gmail.com'),
+(18, 'tes13131312@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -117,7 +151,10 @@ INSERT INTO `orders` (`id`, `user_id`, `total_price`, `created_at`, `updated_at`
 (16, 1, 200.00, '2023-03-25 18:21:14', '2023-03-25 18:21:14', 'Pending'),
 (17, 1, 80.00, '2023-03-25 20:05:05', '2023-03-25 20:05:05', 'Pending'),
 (18, 1, 1000.00, '2023-03-25 20:11:08', '2023-03-25 20:11:08', 'Pending'),
-(19, 1, 1250.00, '2023-03-25 20:14:49', '2023-03-25 20:15:20', 'Delivered');
+(19, 1, 1250.00, '2023-03-25 20:14:49', '2023-03-25 20:15:20', 'Delivered'),
+(20, 1, 800.00, '2023-03-26 15:13:55', '2023-03-26 15:13:55', 'Pending'),
+(21, 1, 20.00, '2023-03-26 19:06:01', '2023-03-26 19:06:01', 'Pending'),
+(22, 1, 175.00, '2023-03-28 07:15:01', '2023-03-28 07:15:01', 'Pending');
 
 -- --------------------------------------------------------
 
@@ -161,7 +198,10 @@ INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price`, 
 (19, 17, 5, 4, 20.00, NULL, NULL),
 (20, 18, 4, 5, 200.00, '2023-03-25 20:11:08', '2023-03-25 20:11:08'),
 (21, 19, 22, 6, 200.00, '2023-03-25 20:14:49', '2023-03-25 20:14:49'),
-(22, 19, 13, 2, 25.00, '2023-03-25 20:14:49', '2023-03-25 20:14:49');
+(22, 19, 13, 2, 25.00, '2023-03-25 20:14:49', '2023-03-25 20:14:49'),
+(23, 20, 4, 4, 200.00, '2023-03-26 15:13:55', '2023-03-26 15:13:55'),
+(24, 21, 5, 1, 20.00, '2023-03-26 19:06:01', '2023-03-26 19:06:01'),
+(25, 22, 9, 7, 25.00, '2023-03-28 07:15:01', '2023-03-28 07:15:01');
 
 -- --------------------------------------------------------
 
@@ -220,12 +260,12 @@ INSERT INTO `products` (`id`, `name`, `description`, `img`, `price`, `stock`, `s
 (1, 'Adriana Grande Live Performance!', 'Ariana Grande is set to take the stage on her tour with a mesmerizing collection of hit songs that will leave audiences spellbound. Her powerhouse vocals and dynamic range will shine on tracks like \"7 Rings\", the irresistible ode to self-empowerment and materialism. The crowd will be swaying and singing along to the catchy chorus of \"Thank U, Next\", a heartfelt expression of gratitude for past relationships. \"Into You\" will bring the heat with its sultry beats and seductive lyrics, while \"God is a Woman\" will be a fierce declaration of female strength and sexuality. Other tour highlights include the pop bop \"Side to Side\" featuring Nicki Minaj, and the soulful ballad \"Love Me Harder\". Get ready to experience the magic of Ariana Grande\'s music, live and in person!', 'Ariana-Grande-2.png', 20.00, 4, 76, 'pop', 'Ariana Grande'),
 (2, 'Justin Bieber\'s Live Show!', 'Justin Bieber is a pop sensation known for his smooth vocals and catchy tunes. Some of his biggest hits include \"Baby\", \"Sorry\", \"Love Yourself\", and \"What Do You Mean?\". With a blend of R&B, pop, and hip-hop influences, Justin\'s music will get you dancing and singing along. He continues to captivate audiences worldwide with his electrifying live performances.', 'Justin-Bieber-2.png', 20.00, 0, 18, 'pop', 'Justin Bieber'),
 (3, 'Dua Lipa on tour!', 'Dua Lipa is a British pop star known for her electrifying vocals and bold fashion sense. Her discography includes chart-topping hits like \"New Rules,\" \"Levitating,\" and \"Don\'t Start Now.\" These empowering anthems, infused with Dua\'s unique blend of pop, R&B, and dance music, are sure to have fans grooving in their seats during her electrifying performances. Whether she\'s singing about self-love and independence or belting out catchy pop hooks, Dua\'s energy and confidence shine through in every note.\r\n', 'Dua-Lipa-2.png', 50.00, 0, 4, 'pop', 'Dua Lipa'),
-(4, 'Billie Eilish to the moon!', 'Billie Eilish is a Grammy-winning singer and songwriter known for her unique, haunting voice and introspective lyrics. She burst onto the music scene with her hit single \"Ocean Eyes\" and has since become one of the biggest names in pop music. Some of her top songs include \"Bad Guy,\" \"When The Party\'s Over,\" and \"Bury A Friend.\" Eilish\'s music is characterized by its ethereal, atmospheric soundscapes and introspective themes that explore the complexities of youth, mental health, and relationships. With her captivating stage presence and powerful performances, Eilish promises to deliver an unforgettable live experience to fans on her upcoming tour.', 'Billie-Eilish-1.png', 200.00, 189, 13, 'pop', 'Billie Eillish'),
-(5, 'Ed\'s Finale ', 'Ed Sheeran is a pop sensation known for his smooth vocals and catchy tunes. Some of his biggest hits include \"Shape of You\", \"Perfect\", \"Thinking Out Loud\", and \"Photograph\". With a blend of R&B, pop, and hip-hop influences, Ed\'s music will get you dancing and singing along. He continues to captivate audiences worldwide with his electrifying live performances.', 'Ed-Sheeran-1.png', 20.00, 168, 42, 'pop', 'Ed Sheeran'),
+(4, 'Billie Eilish to the moon!', 'Billie Eilish is a Grammy-winning singer and songwriter known for her unique, haunting voice and introspective lyrics. She burst onto the music scene with her hit single \"Ocean Eyes\" and has since become one of the biggest names in pop music. Some of her top songs include \"Bad Guy,\" \"When The Party\'s Over,\" and \"Bury A Friend.\" Eilish\'s music is characterized by its ethereal, atmospheric soundscapes and introspective themes that explore the complexities of youth, mental health, and relationships. With her captivating stage presence and powerful performances, Eilish promises to deliver an unforgettable live experience to fans on her upcoming tour.', 'Billie-Eilish-1.png', 200.00, 185, 17, 'pop', 'Billie Eillish'),
+(5, 'Ed\'s Finale ', 'Ed Sheeran is a pop sensation known for his smooth vocals and catchy tunes. Some of his biggest hits include \"Shape of You\", \"Perfect\", \"Thinking Out Loud\", and \"Photograph\". With a blend of R&B, pop, and hip-hop influences, Ed\'s music will get you dancing and singing along. He continues to captivate audiences worldwide with his electrifying live performances.', 'Ed-Sheeran-1.png', 20.00, 167, 43, 'pop', 'Ed Sheeran'),
 (6, 'Metallica\'s Bang', 'Metallica is an American heavy metal band formed in 1981. With their hard-hitting and heavy sound, they have been one of the most influential bands in the genre, paving the way for countless other metal acts. Some of their most popular tracks include \"Enter Sandman,\" \"The Unforgiven,\" \"Master of Puppets,\" and \"Nothing Else Matters.\" When they perform live, they bring their iconic sound and high-energy stage presence, delivering a show that is sure to leave fans headbanging and screaming for more.', 'Metallica-1.png', 50.00, 105, 21, 'metal', 'Metallica'),
 (7, 'To The Iron Side?', 'Iron Maiden is a legendary heavy metal band hailing from England, formed in 1975. With their unique blend of metal, punk, and classical music, they have become one of the most iconic bands in metal history. Their extensive discography includes some of the genre\'s most famous tracks, including \"Hallowed Be Thy Name,\" \"The Trooper,\" and \"Fear of the Dark.\" Iron Maiden is known for their high-energy live shows and iconic stage presence, featuring vocalist Bruce Dickinson\'s powerful voice and guitarist Dave Murray\'s virtuosic playing. If you\'re a fan of metal, Iron Maiden is a must-see live experience.', 'Iron-Maiden-2.png', 50.00, 210, 5, 'metal', 'Iron Maiden'),
 (8, 'Sabbath Days', 'Black Sabbath, one of the pioneers of heavy metal, will surely unleash a thunderous performance of their iconic hits like \"Iron Man,\" \"Paranoid,\" and \"Black Sabbath.\" With their distinctive sound, Black Sabbath will have the crowd headbanging and singing along to classic metal anthems that have stood the test of time. Whether you\'re a longtime fan or a newcomer, a Black Sabbath concert is a must-see event for anyone who loves hard-hitting music with a powerful message.', 'Black-Sabbath-1.png', 10.00, 194, 8, 'metal', 'Black Sabbath'),
-(9, 'Slayer\'s Be Slayin!', 'Slayer is an American thrash metal band that formed in 1981. With their aggressive, fast-paced sound and powerful lyrics, they have become one of the most influential and successful bands in the heavy metal genre. Some of their top tracks include \"Angel of Death,\" \"Raining Blood,\" and \"Seasons in the Abyss.\" The band\'s live shows are renowned for their high energy, and fans can expect to hear these classic tracks, along with others from their extensive catalog, when they perform on tour.', 'Slayer-2.png', 25.00, 200, 2, 'metal', 'Slayer'),
+(9, 'Slayer\'s Be Slayin!', 'Slayer is an American thrash metal band that formed in 1981. With their aggressive, fast-paced sound and powerful lyrics, they have become one of the most influential and successful bands in the heavy metal genre. Some of their top tracks include \"Angel of Death,\" \"Raining Blood,\" and \"Seasons in the Abyss.\" The band\'s live shows are renowned for their high energy, and fans can expect to hear these classic tracks, along with others from their extensive catalog, when they perform on tour.', 'Slayer-2.png', 25.00, 193, 9, 'metal', 'Slayer'),
 (10, 'Judas Contract!', 'Judas Priest is an English heavy metal band formed in Birmingham, England in 1969. They are considered one of the pioneers of the heavy metal genre and have inspired countless musicians and bands over the years. The band is well-known for their powerful vocals, driving guitar riffs, and dynamic song structures. Some of their top picks include \"Breaking the Law,\" \"Painkiller,\" and \"You\'ve Got Another Thing Comin\'.\" Expect a high-energy performance full of headbanging and screaming guitar solos when seeing Judas Priest live.', 'Judas-Priest-2.png', 50.00, 100, 20, 'metal', 'Judas Priest'),
 (11, 'Stormzy On Tour', 'Stormzy, also known as Michael Ebenazer Kwadjo Omari Owuo Jr., is a British grime and hip-hop artist known for his socially conscious lyrics and electrifying live performances. His top tracks include \"Shut Up,\" \"Vossi Bop,\" and \"Own It,\" which showcase his signature smooth flow and thought-provoking lyrics. With his powerful stage presence and politically charged music, Stormzy has become one of the UK\'s leading voices in hip-hop, and continues to inspire and captivate audiences around the world.', 'Stormzy-1.png', 50.00, 400, 80, 'rap', 'Stormzy'),
 (12, 'Skepta teppas tour!', 'Skepta is a British grime rapper and producer, known for his raw and energetic lyrics that often touch on themes of inner-city life, social and political issues, and his struggles and triumphs as a black man in the UK. Some of his top tracks include \"Shutdown,\" \"That\'s Not Me,\" \"Man,\" and \"Konnichiwa.\" Skepta\'s live shows are characterized by his high-energy performances, dynamic beats, and socially conscious lyrics, which have made him a beloved and influential figure in the grime and hip-hop communities. Whether he\'s performing in a packed stadium or an intimate club, Skepta always brings his A-game, delivering powerful, thought-provoking lyrics that resonate with audiences of all ages and backgrounds.', 'Skepa-1.png', 40.00, 200, 2, 'rap', 'Skepta'),
@@ -241,7 +281,7 @@ INSERT INTO `products` (`id`, `name`, `description`, `img`, `price`, `stock`, `s
 (22, 'Dolly Parton\'s Show!', 'Dolly Parton is an American singer, songwriter, and actress known for her country music and her contributions to the entertainment industry. She has been active in the music industry since the 1960s and has released numerous albums throughout her career. Some of Dolly\'s top picks include \"Jolene\", \"I Will Always Love You\", and \"Coat of Many Colors\". She is known for her powerful voice, her ability to bring emotions to life through her music, and her cheerful personality. Her music is a mix of country, pop, and bluegrass, and she has been a major influence on many musicians throughout the years.', 'Dolly-Parton-1.png', 200.00, 14, 16, 'country', 'Dolly Parton'),
 (23, 'Garth Brooks Performance Live', 'Garth Brooks is an American country singer, songwriter and performer known for his signature blend of traditional country, rock and roll and pop music. With a career spanning over 30 years, he is one of the best-selling music artists of all time, with more than 170 million records sold worldwide. Some of his most popular hits include \"The Dance,\" \"Friends in Low Places,\" \"Standing Outside the Fire\" and \"If Tomorrow Never Comes.\" Brooks is recognized for his emotional performances, relatable lyrics, and his energetic stage presence, making him a must-see live performer.', 'Garth-Brooks-1.png', 200.00, 200, 40, 'country', 'Garth Brooks'),
 (24, 'Willie Nelson On Tour!', 'Willie Nelson is a legendary country singer-songwriter known for his distinct voice and contributions to the outlaw country movement. He has released over 200 albums, including the iconic \"Red Headed Stranger\" and \"Stardust.\" Nelson has written numerous hit songs, including \"Crazy,\" \"Always On My Mind,\" and \"On The Road Again.\" He is also known for his activism and support of organizations such as Farm Aid.', 'Willie-Nelson-2.png', 20.00, 200, 20, 'country', 'Willie Nelson'),
-(25, 'George Strait  Live Performance!', 'George Strait is an American country music singer, songwriter, and actor known for his traditional country sound and iconic western wear. He has been active in the industry since the 1970s and has recorded over 60 albums, including classic hits like \"Amarillo by Morning\" and \"All My Ex\'s Live in Texas.\" Strait is considered a living legend in the world of country music and has been referred to as the \"King of Country.\" He is one of the best-selling music artists of all time, with over 100 million records sold worldwide. Despite his immense success, Strait remains true to his roots and continues to perform and record country music that speaks to the heart of his fans.', 'George-Strait-2.png', 10.00, 100, 20, 'country', 'George Strait');
+(25, 'George Strait  Live Performance!', 'George Strait is an American country music singer, songwriter, and actor known for his traditional country sound and iconic western wear. He has been active in the industry since the 1970s and has recorded over 60 albums, including classic hits like \"Amarillo by Morning\" and \"All My Ex\'s Live in Texas.\" Strait is considered a living legend in the world of country music and has been referred to as the \"King of Country.\" He is one of the best-selling music artists of all time, with over 100 million records sold worldwide. Despite his immense success, Strait remains true to his roots and continues to perform and record country music that speaks to the heart of his fans.', 'George-Strait-2.png', 10.00, 50, 20, 'country', 'George Strait');
 
 -- --------------------------------------------------------
 
@@ -305,6 +345,12 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `re
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -372,6 +418,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -387,19 +439,19 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `newsletter`
 --
 ALTER TABLE `newsletter`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
